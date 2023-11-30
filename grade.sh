1b075935facc43e7b5d834b876b18793cc386641
saagrawal04 <97708779+saagrawal04@users.noreply.github.com>
2023-11-29 17:34:04 -0800

    Update grade.sh

--- grade.sh
+++ grade.sh
@@ -8,7 +8,6 @@ mkdir grading-area
 git clone $1 student-submission
 echo 'Finished cloning'
 
-javac student-submission/ListExamples.java
 if [ ! -f student-submission/ListExamples.java ]
 then
   echo "Error: The required Java file ie. ListExamples.java is missing."
@@ -21,15 +20,16 @@ cp grading-area
 cd grading-area
 
 set +e
-javac -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar *.java
+
+javac -cp $CPATH *.java
 if [ $? -ne 0 ]; then
   echo "Error: Compilation failed. Please check your code and try again."
-  exit 
+  exit 1
 fi
 
 
 
-java -cp java -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar org.junit.runner.JUnitCore GradingTestFile > test_output.txt
+java -cp $CPATH org.junit.runner.JUnitCore TestListExamples > test_output.txt
 
 grep "grade" 
 
