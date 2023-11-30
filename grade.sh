bc6ee61a939cac36760a95e62bd304f498a09b15
saagrawal04 <97708779+saagrawal04@users.noreply.github.com>
2023-11-29 16:42:03 -0800

    Update grade.sh

--- grade.sh
+++ grade.sh
@@ -7,10 +7,30 @@ mkdir grading-area
 
 git clone $1 student-submission
 echo 'Finished cloning'
+if [ ! -f student-submission/YourJavaFile.java ]
+then
+  echo "Error: The required Java file is missing in the student submission."
+  exit 
+fi
+
+cp student-submission/YourJavaFile.java grading-area/
+cp path/to/your/grading/tests/* grading-area/
+
+cd grading-area
+set +e
+javac -cp  javac -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar *.java YourJavaFile.java GradingTestFile.java
+if [ $? -ne 0 ]; then
+  echo "Error: Compilation failed. Please check your code and try again."
+  exit 
+fi
 
 
-# Draw a picture/take notes on the directory structure that's set up after
-# getting to this point
 
-# Then, add here code to compile and run, and do any post-processing of the
-# tests
+java -cp java -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar org.junit.runner.JUnitCore GradingTestFile > test_output.txt
+grep "grade" 
+
+
+cd ..
+rm -rf student-submission
+
+
